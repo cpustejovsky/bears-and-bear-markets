b7173087730b71eed6292c6b9ab2f9633ae0ddd7
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-26 19:10:35 -0400

    removed boilerplate; installed and plugged in Bulma.io

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1 className="title">Bears and Bear Markets</h1>
+            </div>
+        )
+    }
+}

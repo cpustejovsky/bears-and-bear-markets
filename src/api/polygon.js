88f73a575d70e852c0e035bb5256493bacd0868c
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-28 13:11:50 -0400

    cleanup

--- src/api/polygon.js
+++ src/api/polygon.js
@@ -8,7 +8,6 @@ const polygon = axios.create({
     apiKey: process.env.REACT_APP_API_KEY
   }
 });
-axios.request(polygon);
 //TODO: check that this work
 export function CheckMarketStatus() {
   const [marketStatus, setMarketStatus] = useState("");
@@ -16,11 +15,8 @@ export function CheckMarketStatus() {
   useEffect(() => {
     (async () => {
       try {
-        //   const response = await axios.get(
-        //     `https://api.polygon.io/marketstatus/now?apiKey=${process.env.REACT_APP_API_KEY}`
-        //   );
-        //   setMarketStatus(response.data.market);
-        setMarketStatus("error");
+        const response = await polygon.get(`/marketstatus/now`);
+        setMarketStatus(response.data.market);
       } catch (error) {
         console.log(error);
         setMarketStatus("error");
@@ -32,8 +28,7 @@ export function CheckMarketStatus() {
 
 export async function FetchStockTickers() {
   const response = await polygon.get(
-    "/v2/reference/tickers?sort=ticker&perpage=50&page=1"
+    "/v2/snapshot/locale/us/markets/stocks/tickers"
   );
-  //   const response = await axios.get('https://api.polygon.io/v2/reference/tickers?sort=ticker&perpage=50&page=1&apiKey=88_aEee41Bvn3eu4F9GU2xgUYi21KInjsGjc_W')
   console.log(response.data);
 }

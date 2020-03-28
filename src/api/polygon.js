b22c209e15d30b0cd3ab473ab83788a02cdbde60
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-28 14:02:58 -0400

    mapping out FetchStockTicker function

--- src/api/polygon.js
+++ src/api/polygon.js
@@ -26,9 +26,13 @@ export function CheckMarketStatus() {
   return marketStatus;
 }
 
-export async function FetchStockTickers() {
-  const response = await polygon.get(
-    "/v2/snapshot/locale/us/markets/stocks/tickers"
-  );
-  console.log(response.data);
+export async function FetchStockTicker(symbol) {
+/*
+    First check day and time
+
+    If the market is open at current day and time, run get request with /v2/snapshot/locale/us/markets/stocks/tickers/{ticker} and return relevant data.
+
+    Otherwise, run /v1/last_quote/stocks/{ticker} and return relevant data.
+*/
+
 }

ce7161df34cb884ed9edee1d09c24fb0ca19a5e0
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-28 21:01:23 -0400

    populated stock ticker with FAANG and some other stocks

--- src/components/StockTicker.jsx
+++ src/components/StockTicker.jsx
@@ -3,15 +3,16 @@ import TickerItem from "./TickerItem";
 export default function StockTicker() {
   return (
     <div className="ticker">
+      <TickerItem symbol={"FB"} />
+      <TickerItem symbol={"AMZN"} />
       <TickerItem symbol={"AAPL"} />
-      <TickerItem symbol={"AAPL"} />
-      <TickerItem symbol={"AAPL"} />
-      <TickerItem symbol={"AAPL"} />
-      <TickerItem symbol={"AAPL"} />
-      <TickerItem symbol={"AAPL"} />
-      <TickerItem symbol={"AAPL"} />
-      <TickerItem symbol={"AAPL"} />
-      <TickerItem symbol={"AAPL"} />
+      <TickerItem symbol={"NFLX"} />
+      <TickerItem symbol={"GOOGL"} />
+      <TickerItem symbol={"INTL"} />
+      <TickerItem symbol={"MSFT"} />
+      <TickerItem symbol={"NVDA"} />
+      <TickerItem symbol={"UBER"} />
+
     </div>
   );
 }
--- src/components/TickerItem.jsx
+++ src/components/TickerItem.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { FetchStockTicker } from "../api/polygon"
+import { FetchStockTicker } from "../api/polygon";
 const TickerItem = ({ symbol }) => {
-  const [name, price] = FetchStockTicker(symbol)
-
-  return (
-    <>
+  const [name, price] = FetchStockTicker(symbol);
+  if (name && price) {
+    return (
+      <>
         <div className="ticker__tick">
-          <h2>{name}: {price} +</h2>
+          <h2>
+            {name}: {price}
+          </h2>
         </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default TickerItem;

6541e0636b85e13c13a1122cbddfe99864cfe6c7
cpustejovsky <charles.pustejovsky@gmail.com>
2020-03-29 13:18:00 -0400

    adding settingsmodal back in (accidental removal in prior commit)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PolygonIO from "../images/polygonio_white.svg";
-import SettingsModal from "./SettingsModal"
+import SettingsModal from "./SettingsModal";
 const NavBar = () => {
   const [navbar, setNavbar] = useState("navbar-menu");
   const [burger, setBurger] = useState("navbar-burger burger");
@@ -19,66 +19,70 @@ const NavBar = () => {
 
   return (
     <>
-    <nav
-      className="navbar is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a
-            href="https://polygon.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="navbar-item"
-          >
-            <img
-              className="navbar__logo"
-              alt="polygon.io logo"
-              src={PolygonIO}
-            />
-          </a>
-          <a className="navbar-item" href="/">
-            Bears and Bear Markets
-          </a>
+      <SettingsModal settingsModal={settingsModal} onClose={setSettingsModal} />
+      <nav
+        className="navbar is-primary"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <a
+              href="https://polygon.io/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="navbar-item"
+            >
+              <img
+                className="navbar__logo"
+                alt="polygon.io logo"
+                src={PolygonIO}
+              />
+            </a>
+            <a className="navbar-item" href="/">
+              Bears and Bear Markets
+            </a>
 
-          <a
-            onClick={() => openMobileNav()}
-            role="button"
-            className={burger}
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
+            <a
+              onClick={() => openMobileNav()}
+              role="button"
+              className={burger}
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
 
-        <div id="navbarBasicExample" className={navbar}>
-          <div className="navbar-start">
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">About</a>
+          <div id="navbarBasicExample" className={navbar}>
+            <div className="navbar-start">
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">About</a>
 
-              <div className="navbar-dropdown">
-                <p className="navbar__about">
-                  Being invested often leaves you feeling, well, invested. And
-                  when the value of your invetments drop, it's easy to become
-                  stressed. So this site allows you to view your assets while
-                  also viewing a relaxing nature live feed of bears to calm you
-                  through any bear markets.
-                </p>
+                <div className="navbar-dropdown">
+                  <p className="navbar__about">
+                    Being invested often leaves you feeling, well, invested. And
+                    when the value of your invetments drop, it's easy to become
+                    stressed. So this site allows you to view your assets while
+                    also viewing a relaxing nature live feed of bears to calm
+                    you through any bear markets.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div onClick={()=>setSettingsModal(!settingsModal)} className="navbar-end navbar__settings">
-            <i className="fas fa-cog fa-lg"></i>
-            <span className="navbar__settings__mobile"> Settings</span>
+            <div
+              onClick={() => setSettingsModal(!settingsModal)}
+              className="navbar-end navbar__settings"
+            >
+              <i className="fas fa-cog fa-lg"></i>
+              <span className="navbar__settings__mobile"> Settings</span>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </>
   );
 };

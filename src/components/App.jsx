6a32e60d09db80dec57c4722b58199381b6ec75b
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-05 15:09:12 -0400

    setting up router in components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Display from "./Display";
+import Settings from "./Settings";
 import Footer from "./Footer";
+import history from "../history";
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <NavBar />
-        <Display />
+        <Router history={history}>
+          <NavBar />
+          <>
+            <Switch>
+              <Route path="/" exact component={Display} />
+              <Route path="/settings" exact component={Settings} />
+            </Switch>
+          </>
+        </Router>
         <Footer />
       </div>
     );
---
+++ src/components/Settings.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Settings extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This will be the settings page!</h1>
+            </div>
+        )
+    }
+}

606f644719329abbb6a4a3f2796a0d7fb6408b07
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-06 09:05:20 -0400

    setting up default ticker arrays

--- src/reducers/tickerReducer.js
+++ src/reducers/tickerReducer.js
@@ -1,16 +1,10 @@
-const defaultTickers = [
-  "FB",
-  "AMZN",
-  "AAPL",
-  "NFLX",
-  "GOOGL",
-  "INTL",
-  "MSFT",
-  "NVDA",
-  "UBER",
-];
+const defaultTickers = {
+  faang: ["FB", "AMZN", "AAPL", "NFLX", "GOOGL"],
+  otherTech: ["INTL", "MSFT", "NVDA", "UBER", "LYFT"],
+  auto: ["TSLA", "TM", "HMC", "GM", "F"],
+};
 //TODO: hook up to a redux form in settings
-export const tickersReducer = (state = defaultTickers, action) => {
+export const tickersReducer = (state = defaultTickers.auto, action) => {
   switch (action.type) {
     case "FETCH_TICKERS":
       return action.payload;

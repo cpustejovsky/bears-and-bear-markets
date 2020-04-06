67779f399344c4b307aa3516c499d7f7e6f15410
cpustejovsky <charles.pustejovsky@gmail.com>
2020-04-06 18:27:15 -0400

    rudimentary settings working; reducer fix

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { selectTickers } from "../actions";
+import history from "../history";
 
 class Settings extends Component {
-  change = (event) => {
+  state = {
+    term: "",
+  };
+  changeDefaultTickers = (event) => {
     this.props.selectTickers(event.target.value);
-  }
+  };
   render() {
     return (
       <div className="container margin-top">
         <div className="columns">
           <div className="column is-half is-offset-one-quarter">
             <h1 className="title">Settings</h1>
-            <h1 className="subtitle">Currently a work in progress (WIP)</h1>
             <div className="field">
               <label className="label">Select Default Tickers</label>
               <div className="control">
                 <div className="select">
-                  <select onChange={this.change}>
+                  <select onChange={this.changeDefaultTickers}>
                     <option value="faang">FAANG</option>
                     <option value="otherTech">Other Tech Companies</option>
                     <option value="auto">Automotive Companies</option>
@@ -25,17 +28,34 @@ class Settings extends Component {
                 </div>
               </div>
             </div>
-            <div className="field">
-              <label className="label">
-                Search for Stock or Exchange by Symbol
-              </label>
+            <label className="label">
+              Search for Stock or Exchange by Symbol
+            </label>
+            <div className="field has-addons">
               <div className="control">
-                <input className="input" type="text" placeholder="AAPL" />
+                <input
+                  className="input"
+                  type="text"
+                  placeholder="AAPL"
+                  value={this.state.term}
+                  onChange={this.onInputChange}
+                />
+              </div>
+              <div class="control">
+                <a class="button is-success">Search</a>
               </div>
             </div>
+            <div className="divider"/>
             <div className="field is-grouped">
               <div className="control">
-                <button className="button is-primary">Submit</button>
+                <button
+                  className="button is-primary"
+                  onClick={() => {
+                    history.push("/");
+                  }}
+                >
+                  Submit
+                </button>
               </div>
               <div className="control">
                 <button className="button is-primary is-light">Cancel</button>
